7a419528d20fe309be5acfbe43e26b3711a757ef
ZzukMa <67806878+minhyuuk@users.noreply.github.com>
2021-03-19 23:49:52 +0900

    Initial commit

---
+++ app/src/main/java/com/example/school_lunch/Lunch.kt
@@ -0,0 +1,11 @@
+package com.example.school_lunch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Lunch : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lunch)
+    }
+}
