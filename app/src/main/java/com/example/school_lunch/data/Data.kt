d4465d91290959ae9c1befeb7ce607c0a3af51fb
ZzukMa <67806878+minhyuuk@users.noreply.github.com>
2021-03-22 17:51:08 +0900

    icon

---
+++ app/src/main/java/com/example/school_lunch/data/Data.kt
@@ -0,0 +1,3 @@
+package com.example.school_lunch.data
+
+data class lunch(val date:String,val menu:String,val day:String)
--- app/src/main/java/com/example/school_lunch/retrofit/RetrofitInteface.kt
+++ app/src/main/java/com/example/school_lunch/retrofit/RetrofitInteface.kt
@@ -1,7 +1,10 @@
 package com.example.school_lunch.retrofit
 
+import com.example.school_lunch.data.lunch
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface RetrofitInteface {
-
+    @GET("open.neis.go.kr/hub/mealServiceDietInfo")
+    fun  ApiService(): Call<lunch>
 }
