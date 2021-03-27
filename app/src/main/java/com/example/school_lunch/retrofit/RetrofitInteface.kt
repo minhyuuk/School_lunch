2b51252e937308ba996079518fc03826d13e574b
ZzukMa <67806878+minhyuuk@users.noreply.github.com>
2021-03-27 23:39:38 +0900

    change api key

--- app/src/main/java/com/example/school_lunch/retrofit/RetrofitInteface.kt
+++ app/src/main/java/com/example/school_lunch/retrofit/RetrofitInteface.kt
@@ -16,7 +16,8 @@ interface RetrofitInteface {
             @Query("MMEAL_SC_CODE") num: Int, // 아침 점심 저녁, 1,2,3
             @Query("ATPT_OFCDC_SC_CODE") gwangju: String = "F10", // 광주 교육청 코드,  F10
             @Query("SD_SCHUL_CODE") school: Int = 7380292, // 표준 학교 코드, 7380292
-            @Query("KEY") key: String = "9e67bf29119b4c5389bec9404585ad45", // api 인증키, 9e67bf29119b4c5389bec9404585ad45
+
+            @Query("KEY") key: String = "67ebdc05dfea4d1dbc9fe76fc5a7932b", // api 인증키, 9e67bf29119b4c5389bec9404585ad45
             @Query("Type") Type: String = "json", // json 타입
             @Query("pIndex") pIndex: Int? = 1, // 페이지 위치            |   기본값 : 1(sample key는 1 고정)
             @Query("psize") pSize: Int? = 100  // 페이지 당 신청 숫자     |  기본값 : 100(sample key는 5 고정)
